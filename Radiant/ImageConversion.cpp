bde6fb89829bb324f97495ac8db3627ae5bff3a1
Tommi.Ilmonen <Tommi.Ilmonen@d2709d7f-9d48-0410-97af-374aaeb070bb>
2008-03-31 18:52:58 +0000

    Updated license texts in the C++ files.
    git-svn-id: http://multitude.googlecode.com/svn/trunk@28 d2709d7f-9d48-0410-97af-374aaeb070bb

--- Radiant/ImageConversion.cpp
+++ Radiant/ImageConversion.cpp
@@ -1,10 +1,10 @@
 /* COPYRIGHT
  *
- * This file is part of Diva.
+ * This file is part of Radiant.
  *
  * Copyright: Helsinki University of Technology, MultiTouch Oy and others.
  *
- * See file "Diva.hpp" for authors and more details.
+ * See file "Radiant.hpp" for authors and more details.
  *
  * This file is licensed under GNU Lesser General Public
  * License (LGPL), version 2.1. The LGPL conditions can be found in 
--- Screenplay/Screenplay.hpp
+++ Screenplay/Screenplay.hpp
@@ -1,10 +1,10 @@
 /* COPYRIGHT
  *
- * This file is part of DivaIO.
+ * This file is part of Screenplay.
  *
  * Copyright: Helsinki University of Technology, MultiTouch Oy and others.
  *
- * See file "DivaIO.hpp" for authors and more details.
+ * See file "Screenplay.hpp" for authors and more details.
  *
  * This file is licensed under GNU Lesser General Public
  * License (LGPL), version 2.1. The LGPL conditions can be found in 
