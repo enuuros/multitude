46f38968ceba21cbdffdedccbbe86be024586ec8
esa.nuuros <esa.nuuros@d2709d7f-9d48-0410-97af-374aaeb070bb>
2008-04-29 08:31:36 +0000

    Some cleanup
    git-svn-id: http://multitude.googlecode.com/svn/trunk@93 d2709d7f-9d48-0410-97af-374aaeb070bb

--- Radiant/FileUtils.cpp
+++ Radiant/FileUtils.cpp
@@ -116,7 +116,7 @@ namespace Radiant
       for(StringList::iterator it = pathList.begin(); it != pathList.end(); it++) {
         string fullPath = (*it) + string("/") + filename;
 
-        trace("TRYING %s", fullPath.c_str());
+        trace("FileUtils::findFile # TRYING %s", fullPath.c_str());
 
         if(fileReadable(fullPath.c_str()))
           return fullPath;
--- Radiant/StandardPaths.hpp
+++ Radiant/StandardPaths.hpp
@@ -9,19 +9,6 @@ namespace Radiant
   /// Default font search paths (no trailing /) 
   const std::string STD_FONT_PATHS("../Fonts:/usr/share/fonts/truetype/ttf-dejavu:.");
 
-/*
-  class Path
-  {    
-    void addPath(const std::string & path);
-    const std::string & path() const;
-
-    /// Find the given file in all registered paths
-    /// @param filename name of the file
-    /// @return full path to the file, empty if not found
-    std::string findFile(const std::string & filename) const;
-  };
-*/
- 
 }
 
 #endif
