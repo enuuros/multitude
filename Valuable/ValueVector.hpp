aa125c342a072a6ca46c142b9afe533b2192c02b
esa.nuuros <esa.nuuros@d2709d7f-9d48-0410-97af-374aaeb070bb>
2009-02-11 09:05:41 +0000

    Cleaning old comments
    git-svn-id: http://multitude.googlecode.com/svn/trunk@463 d2709d7f-9d48-0410-97af-374aaeb070bb

--- Valuable/ValueVector.hpp
+++ Valuable/ValueVector.hpp
@@ -75,17 +75,6 @@ namespace Valuable
   typedef ValueVector<Nimble::Vector3f, float, 3> ValueVector3f;
   typedef ValueVector<Nimble::Vector4f, float, 4> ValueVector4f;
 
-  // Instantiations of template classes
-
-  /* Moved to a C++ file.
-  template class ValueVector<Nimble::Vector2f, float, 2>;
-  template class ValueVector<Nimble::Vector3f, float, 3>;
-  template class ValueVector<Nimble::Vector4f, float, 4>;
-
-  template class ValueVector<Nimble::Vector2i, int, 2>;
-  template class ValueVector<Nimble::Vector3i, int, 3>;
-  template class ValueVector<Nimble::Vector4i, int, 4>;
-  */
 }
 
 #undef STD_OP
