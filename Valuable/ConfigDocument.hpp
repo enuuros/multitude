7ce8166d127d7e3da10c68b3ffbacad5bb2389ce
ekhader <ekhader@d2709d7f-9d48-0410-97af-374aaeb070bb>
2009-08-11 10:11:54 +0000

    git-svn-id: http://multitude.googlecode.com/svn/trunk@687 d2709d7f-9d48-0410-97af-374aaeb070bb

---
+++ Valuable/ConfigDocument.hpp
@@ -0,0 +1,26 @@
+#include "Element.hpp"
+#include <fstream>
+class ConfigDocument
+{
+public:
+	ConfigDocument(void);
+	~ConfigDocument(void);
+
+	void readConfigFile(char *fileName);
+	void writeConfigFile(char *fileName);
+	Element Elements;
+	
+	
+private:
+	void loadElement(string str);
+	
+	void loadAttribute(string key,string val);
+	string getConfigText(Element e,vector<string> &ss);
+	
+	enum PARSE_FLAGS
+	{
+		ELEMENT_START,ELEMENT_END,ATTRIBUTE,NOT_VALID
+
+	};
+	PARSE_FLAGS parseLine(string line);
+};
---
+++ Valuable/Element.hpp
@@ -0,0 +1,19 @@
+#include "Attribute.hpp"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+class Element
+{
+public:
+	Element(void);
+	~Element(void);
+	vector<Attribute> Attributes;
+	vector<Element> Nodes;
+	string type;
+	int depth;
+	string elementName;
+
+
+};
