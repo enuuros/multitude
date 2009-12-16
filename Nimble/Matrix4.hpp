53e5475987b49f7ef462193a496f515e663a9db4
Tommi.Ilmonen <Tommi.Ilmonen@d2709d7f-9d48-0410-97af-374aaeb070bb>
2009-12-16 07:14:32 +0000

    Nimble Windows fix.
    git-svn-id: http://multitude.googlecode.com/svn/trunk@902 d2709d7f-9d48-0410-97af-374aaeb070bb

--- Nimble/Matrix4.hpp
+++ Nimble/Matrix4.hpp
@@ -88,7 +88,7 @@ namespace Nimble {
 
     static int                rows() { return 4; }
     static int                columns() { return 4; }
-    inline static void        test();
+    NIMBLE_API static void    test();
 
     T *       data()       { return m[0].data(); }
     const T * data() const { return m[0].data(); }
@@ -100,8 +100,8 @@ namespace Nimble {
 
     static Matrix4T<T> makeRotation(T radians, const Vector3T<T> & axis);
     static Matrix4T<T> makeTranslation(const Vector3T<T> & v);
-    static Matrix4T<T> translate3D(const Vector3T<T> & v);
-    static Matrix4T<T> scale3D(const Vector3T<T> & v);
+    NIMBLE_API static Matrix4T<T> translate3D(const Vector3T<T> & v);
+    NIMBLE_API static Matrix4T<T> scale3D(const Vector3T<T> & v);
 
     /** Identity matrix. */
     NIMBLE_API static const Matrix4T<T> IDENTITY;
@@ -111,88 +111,6 @@ namespace Nimble {
 
     Vector4T<T> m[4];
   };
-
-  template <class T>
-  inline void Matrix4T<T>::test()
-  {
-    Matrix4T<T> a;
-    int i,j;
-    /* STORING & INDEXING ELEMENTS */
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    a[i][j] = T(4*i+j);
-
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    assert(a[i][j] == 4*i+j);
-
-    /* CLEAR */
-    a.clear();
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    assert(a[i][j] == 0);
-
-    /* ROW & COLUMN OPERATORS */
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    a[i][j] = T(4*i+j);
-
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    {
-      assert(a.row(i)[j] == 4*i+j);
-      assert(a.column(j)[i] == 4*i+j);
-    }
-    /* TRANSPOSE */
-    a.transpose();
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    assert(a[j][i] == 4*i+j);
-
-    /* IDENTITY */
-    a.identity();
-
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    {
-      if( i == j ) assert(a[i][j] == 1);
-      else assert(a[i][j] == 0);
-    }
-
-    /* COPY OPERATOR, CONSTRUCTOR AND EQUALITY OPERATOR */
-
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    a[i][j] = T(4*i+j);
-
-    Matrix4T<T> b(a);
-    assert(a == b);
-    assert(!(a != b));
-
-    /* MATRIX MULTIPLICATION */
-
-    Matrix4T<T> c;
-    c.identity();
-    b *= c;
-    assert(a == b);
-
-    c.clear();
-    b *= c;
-    for(i = 0; i < 4; i++)
-      for(j = 0; j < 4; j++)
-    assert(b[i][j] == 0);
-
-    /* Test setRotation/getRotation */
-
-    Matrix3T<T> a3,b3;
-    for(i = 0; i < 3; i++)
-      for(j = 0; j < 3; j++)
-    a[i][j] = T(3*i+j);
-
-    a.setRotation(a3);
-    b3 = a.getRotation();
-    assert(a3 == b3);
-  }
 
   template <class T>
   inline void Matrix4T<T>::swap(T &a, T& b)
