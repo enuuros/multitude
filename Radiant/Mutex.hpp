02e568f3057928423e797c8ccaec191b03f7da70
esa.nuuros <esa.nuuros@d2709d7f-9d48-0410-97af-374aaeb070bb>
2009-12-29 10:11:16 +0000

    Made mutex be non-recursive by default
    git-svn-id: http://multitude.googlecode.com/svn/trunk@937 d2709d7f-9d48-0410-97af-374aaeb070bb

--- Radiant/Mutex.hpp
+++ Radiant/Mutex.hpp
@@ -48,7 +48,7 @@ namespace Radiant {
     Recursion should work on all platforms. */
     bool init(bool shared = false, 
 	      bool prio_inherit = true,
-	      bool recursive = true);
+	      bool recursive = false);
   
     /// Close the mutex.
     bool close();
