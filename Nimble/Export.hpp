ee8b941ed594faff3e8fa22b2ee897b2975bfe7c
Tommi.Ilmonen <Tommi.Ilmonen@d2709d7f-9d48-0410-97af-374aaeb070bb>
2010-01-02 20:33:53 +0000

    Reduced dll-linkage warnings with MSVC, using pragmas.
    git-svn-id: http://multitude.googlecode.com/svn/trunk@941 d2709d7f-9d48-0410-97af-374aaeb070bb

--- Nimble/Export.hpp
+++ Nimble/Export.hpp
@@ -7,16 +7,18 @@
  * See file "Nimble.hpp" for authors and more details.
  *
  * This file is licensed under GNU Lesser General Public
- * License (LGPL), version 2.1. The LGPL conditions can be found in 
- * file "LGPL.txt" that is distributed with this source package or obtained 
+ * License (LGPL), version 2.1. The LGPL conditions can be found in
+ * file "LGPL.txt" that is distributed with this source package or obtained
  * from the GNU organization (www.gnu.org).
- * 
+ *
  */
 
 #ifndef NIMBLE_EXPORT_HPP
 #define NIMBLE_EXPORT_HPP
 
 #ifdef WIN32
+
+#pragma warning( disable : 4275 )
 
 // Import by default
 #ifdef NIMBLE_EXPORT
@@ -27,6 +29,6 @@
 
 #else
 #define NIMBLE_API
-#endif  
+#endif
 
 #endif
--- Radiant/Export.hpp
+++ Radiant/Export.hpp
@@ -7,16 +7,18 @@
  * See file "Radiant.hpp" for authors and more details.
  *
  * This file is licensed under GNU Lesser General Public
- * License (LGPL), version 2.1. The LGPL conditions can be found in 
- * file "LGPL.txt" that is distributed with this source package or obtained 
+ * License (LGPL), version 2.1. The LGPL conditions can be found in
+ * file "LGPL.txt" that is distributed with this source package or obtained
  * from the GNU organization (www.gnu.org).
- * 
+ *
  */
 
 #ifndef RADIANT_EXPORT_HPP
 #define RADIANT_EXPORT_HPP
 
 #ifdef WIN32
+
+#pragma warning( disable : 4275 )
 
 // Import by default
 #ifdef RADIANT_EXPORT
@@ -27,6 +29,6 @@
 
 #else
 #define RADIANT_API
-#endif  
+#endif
 
 #endif
